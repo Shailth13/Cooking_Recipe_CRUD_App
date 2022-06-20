7bedd5f02f99b62006934ffe3fb761db5954e9da
Shailth13 <97862443+Shailth13@users.noreply.github.com>
2022-06-20 03:29:45 -0500

    completed User Login auth with JWT

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,25 @@
-#main file just like in the ML project to initializing the project
-
-from ast import Delete
-from datetime import date
-from dis import Instruction
-from unicodedata import category
-from flask import Flask,request
+# main file just like in the ML project to initializing the project
+from os import access
+from flask import Flask, jsonify,request,jsonify
 from flask_restx import Api,Resource,fields
+import jwt
 from config import DevConfig
-from  models import Recipe
+from models import Recipe,User
 from exts import db
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager,create_access_token,create_refresh_token,jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app=Flask(__name__)
 app.config.from_object(DevConfig)
 
 # registers SQL_ALCHEMY to work with our application
 db.init_app(app)
+
+# to integrate flask_migrate with our application
+migrate=Migrate(app,db)
+
+JWTManager(app)
 
 # this is the path to our swagger document.
 api=Api(app,doc='/docs')
@@ -35,24 +40,100 @@ recipe_model=api.model(
     }
 )
 
+signup_model=api.model(
+    'SignUp',
+    {
+        "username":fields.String(),
+        "email":fields.String(),
+        "password":fields.String()
+    }
+)
+
+signin_model=api.model(
+    'SignIn',
+    {
+        "username":fields.String(),
+        "password":fields.String()
+    }
+)
+
+
 @api.route('/fetch')
 class FetchResource(Resource):
     def get(self):
         return {"message":"fetching resources"}
 
-#Creating various CRUD Routes:
+# Creating routes for User LOGIN -> using JWT 
+@api.route('/signup')
+@api.expect(signup_model)
+class SignUp(Resource):
+    # @api.marshal_with(signup_model)
+    def post(self):
+        # this data is the data sent by client using the post method.
+        # here we are going to make use of werkzeug library to check and encrypt our password using hash code
+        data=request.get_json()
+
+        # exception handling if user already exists in db
+        # getting from client request
+        username=data.get('username') 
+        # getting from querying the database
+        db_user=User.query.filter_by(username=username).first() 
+
+        if db_user is not None:
+            return jsonify({"message":f"User with username {username} is taken."})
+
+        new_user=User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=generate_password_hash(data.get('password'))
+        )
+
+        new_user.save()
+
+        # lets check the API in Insomnia 
+        # instead of returning new user object with credentials we will simply return success message
+        # return new_user,201 
+        return jsonify({"message":"User Successfully created"})
+
+@api.route('/signin')
+@api.expect(signin_model)
+class SignIn(Resource):
+    def post(self):
+        data=request.get_json()
+
+        username=data.get('username')
+        password=data.get('password')
+
+        db_user=User.query.filter_by(username=username).first()
+
+        # Now to check if the user exists in the db and authenticate the user.
+        # we will be using two additional functions 
+        if db_user and check_password_hash(db_user.password,password):
+
+            access_token=create_access_token(identity=db_user.username)
+            refresh_token=create_refresh_token(identity=db_user.username)
+
+            return jsonify({
+                "access token":access_token,"refresh_token":refresh_token
+            })
+
+
+# Creating various CRUD Routes:
 
 @api.route('/recipes')
 class CookingRecipes(Resource):
     @api.marshal_list_with(recipe_model)
     def get(self):
         """Get all recipes from db (Read all operation)"""
-        recipes=Recipe.query.all() #which returns a SQLAlchemy object and we convert it to JSON using marshal serializer model. Goto insomnia and test the API using a new GET request "Get_All_Recipes"
+        recipes=Recipe.query.all() #which returns a SQLAlchemy object and we convert it to JSON using marshal serializer model which is a decorator. Goto insomnia and test the API using a new GET request "Get_All_Recipes"
         
         return recipes
 
 
     @api.marshal_with(recipe_model)
+    # the expect decorator helps with showing the payload for a given contract model.
+    @api.expect(recipe_model)
+    @jwt_required()
     def post(self):
         """Create a new recipe (add/Create operation)"""
         #here we are going to use the flask request method to create a new row by acquiring data that comes as request from client.
@@ -75,17 +156,39 @@ class CookingRecipes(Resource):
 
 @api.route('/recipe/<int:id>')
 class CookingRecipes(Resource):
+
+    @api.marshal_with(recipe_model)
     def get(self,id):
         """Get a recipe by id(Read one operation)"""
-        pass
+        recipe=Recipe.query.get_or_404(id) # the query checks for a recipe with given id and if not found returns a 404 error
+        # create a new GET request in insomnia (get_one_recipe) and localhost:5000/recipe/1. We should get a recipe with id 1
+          
+        return recipe
 
+    @api.marshal_with(recipe_model)
+    @jwt_required()
     def put(self,id):
         """update a recipe by id(Update operation)"""
-        pass
+        # first we check if the recipe that is queried for update exists in the database
+        recipe_to_update=Recipe.query.get_or_404(id)
+        #next we fetch the data sent as request by the client and update the target recipe in the database
+        mod_data=request.get_json()
+        # for update operation we will not use or allow date_added to be part of request object or query object.
+        #date_added is created by default by the model system at time of creation of the recipe just like id.
+        recipe_to_update.update(mod_data.get('name'),mod_data.get('ingredients'),mod_data.get('instructions'),mod_data.get('serving_size'),mod_data.get('category'),mod_data.get('notes'),mod_data.get('date_modified'))
 
+        #lets test it in insomnia using new put request update_a_recipe. here date modified should take in system date time to reflect the current time of update operation.
+
+        return recipe_to_update
+
+    @api.marshal_with(recipe_model)
+    @jwt_required()
     def delete(self,id):
         """delete a recipe by id(delete operation)"""
-        pass
+        recipe_to_delete=Recipe.query.get_or_404(id)
+        recipe_to_delete.delete()
+        # next we test our delete operation in insomnia with a delete_a_recipe DELETE request
+        return recipe_to_delete
 
 # used in python shell and uses SQLAlchemy engine to create Recipe db
 @app.shell_context_processor
--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from exts import db
+from sqlalchemy.orm import registry
 
 """data model
 
@@ -26,6 +27,7 @@ class Recipe(db.Model):
 
     # we will be writing methods for CRUD operations next
     def __repr__(self):
+        # string representation of the object Recipe > name
         return f"<Recipe {self.name} >"
 
     def save(self):
@@ -36,16 +38,46 @@ class Recipe(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def update(self, name, ingredients, instructions,serving_size,category,notes,date_added,date_modified):
+    def update(self, name, ingredients, instructions,serving_size,category,notes,date_modified):
         self.name = name
         self.ingredients = ingredients
         self.instructions = instructions
         self.serving_size = serving_size
         self.category = category
         self.notes = notes
-        self.date_added = date_added
         self.date_modified = date_modified
+        db.session.commit()
 
 # expose this model using the serializer on our API
+
+"""Login User Model
+
+class User:
+    id=int primary key
+    username=string
+    email=string
+    password=text
+"""
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(25),nullable=False,unique=True)
+    email=db.Column(db.String(80),nullable=False)
+    password=db.Column(db.Text(),nullable=False)
+
+    #After this we will be craeting a convenience method that is going to help us create a password for user.
+    # Or we may choose to ignore it for now and create it in our routes in main.py
+
+    #string representation of the object username
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
 
 
